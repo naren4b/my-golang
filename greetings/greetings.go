b929b08507d5bd1fd48fce66d12bb9233f455301
Narendranath Panda <narendranath.panda@nokia.com>
2026-03-16 06:45:18 +0530

    Add greetings package with Hello function and main application

---
+++ greetings/greetings.go
@@ -0,0 +1,28 @@
+package greetings
+
+import (
+    "fmt"
+    "errors"
+    "math/rand"
+)
+// Hello returns a greeting for the named person.
+func Hello(name string) (string,error) {
+    if name == "" {
+        return "", errors.New("empty name")        
+    }
+
+   
+
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf(randomFormat(), name)
+    return message,nil
+}
+
+func randomFormat() string {
+     messages := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+    return messages[rand.Intn(len(messages))]
+}
---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "example.com/greetings"
+)
+
+func main() {
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+    // Get a greeting message and print it.
+    message,err := greetings.Hello("Naren")
+    if err !=nil{
+        log.Fatal(err)
+    }
+    fmt.Println(message)
+}
